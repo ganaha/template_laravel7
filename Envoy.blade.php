d07403aa94c46ce21f64a2ef8e81590145ded4d6
t.ganaha <t.ganaha@lexues.co.jp>
2020-07-09 10:17:10 +0900

    Update log permission

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,30 +3,28 @@
 @story('deploy')
     git
     composer
-    npm
     cache
     migrate
 @endstory
 
 @task('git')
-    cd /opt/bitnami/apache2/htdocs
+    cd /opt/bitnami/apache2/htdocs/laravel
     git pull origin master
 @endtask
 
 @task('composer')
+    cd /opt/bitnami/apache2/htdocs/laravel
     composer install --optimize-autoloader --no-dev
 @endtask
 
-@task('npm')
-    npm install
-@endtask
-
 @task('cache')
+    cd /opt/bitnami/apache2/htdocs/laravel
     php artisan config:cache
-    php artisan route:cache
+    {{-- php artisan route:cache --}}
     php artisan view:cache
 @endtask
 
 @task('migrate')
+    cd /opt/bitnami/apache2/htdocs/laravel
     php artisan migrate
 @endtask
